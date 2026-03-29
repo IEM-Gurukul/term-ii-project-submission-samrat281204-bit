e110fec1a9904672f8bf4f41618386b464b313c2
samrat281204-bit <samrat281204@gmail.com>
2026-03-29 21:36:33 +0530

    Create Person class with id and name attributes

---
+++ src/model/Person.java
@@ -0,0 +1,20 @@
+package model;
+
+public class Person {
+
+    protected int id;
+    protected String name;
+
+    public Person(int id, String name) {
+        this.id = id;
+        this.name = name;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+}
